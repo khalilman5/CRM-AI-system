e8b9af4c9534d8f6a749f299912872330f30f50f
khalilman5 <109165812+khalilman5@users.noreply.github.com>
2026-08-11 17:27:32 +0100

    create folder

--- test_module/models/dashboard.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class TestDashboard(models.Model):
-    _name = 'test_module.dashboard'
-    _description = 'Test Dashboard'
-
-    name = fields.Char(required=True)
